dd35aa24518d364839cc1bde6f0cf7663eb6865e
Preston Kwei <prestonkwei@gmail.com>
2025-01-02 23:33:22 -0800

    edits

--- info-inject.js
+++ info-inject.js
@@ -1,5 +1,5 @@
 /** @type {typeof import("./static.json")} */
-const data = await fetch("/static.json").then((x) => x.json());
+const data = await fetch('/static.json').then((x) => x.json())
 
-const update = data.update-date;
+const update = data.update - date
 console.log(update)
